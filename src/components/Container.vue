60812ab95797ea515ce22de010748e54a1e56b22
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2021-07-15 22:04:40 +0530

    1 file modified

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,16 +1,19 @@
 <template>
 <div class="container">
+<div class="block">
 <div>
-Block 1
+<div>Small</div>
+<div>Medium</div>
+<div>Large</div>
+</div>
 </div>
 <hr/>
-<div>
-Block 2
-{{name}}
+<div class="block">
+<div>Adults</div>
 </div>
 <hr/>
-<div>
-Block 3
+<div class="block">
+<div>Children</div>
 </div>
 </div>
 </template>
@@ -19,7 +22,11 @@ export default {
   name : "Container",
   data(){
     return{
-name : "nn"
+small:0,
+medium:0,
+large:0,
+adults:0,
+children:0
     }
   }
 }
@@ -34,5 +41,8 @@ name : "nn"
 }
 hr{
   width:90%
+}
+.block{
+  display:flex;
 }
 </style>
