536ccca3706585fcebded75d1bbea7840d0fb1fd
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2021-07-15 21:33:34 +0530

    2 files modified, 1 deleted

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,16 @@
 <template>
+<div class="container">
 <div>
-
+Block 1
+</div>
+<hr/>
+<div>
+Block 2
+</div>
+<hr/>
+<div>
+Block 3
+</div>
 </div>
 </template>
 <script>
@@ -8,3 +18,15 @@ export default {
   name : "Container",
 }
 </script>
+
+<style>
+.container{
+  border : 1px solid blue;
+  border-radius:2px;
+  display:flex;
+  flex-direction:column;
+}
+hr{
+  width:90%
+}
+</style>
